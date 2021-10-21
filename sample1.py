c7c9bfe1a70477cab8be96889e5e7958850c38e9
Sujin0722 <hjhjehj@ajou.ac.kr>
2021-10-21 21:40:38 +0900

    2908

--- sample1.py
+++ sample1.py
@@ -1,1 +1,12 @@
-print("hello")
+A, B= input().split()
+
+A_l = list(A)
+B_l = list(B)
+
+A = int(A_l[2])*100+int(A_l[1])*10+int(A_l[0])
+B = int(B_l[2])*100+int(B_l[1])*10+int(B_l[0])
+
+if A>B:
+    print(A)
+else:
+    print(B)
