fc03262c20208a5952e39b3100471e2a5ef3aaac
Sujin0722 <hjhjehj@ajou.ac.kr>
2021-10-22 14:17:18 +0900

    다일얼

--- sample1.py
+++ sample1.py
@@ -1,12 +1,10 @@
-A, B= input().split()
+num = list(input())
+alpha = ['ABC', 'DEF','GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+count = 0
 
-A_l = list(A)
-B_l = list(B)
+for i in range(len(num)):
+    for j in range(len(alpha)):
+        if num[i] in alpha[j]:
+            count = count+(j+3)
 
-A = int(A_l[2])*100+int(A_l[1])*10+int(A_l[0])
-B = int(B_l[2])*100+int(B_l[1])*10+int(B_l[0])
-
-if A>B:
-    print(A)
-else:
-    print(B)
+print(count)
